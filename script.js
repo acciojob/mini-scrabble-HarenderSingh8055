ef36b26686649588cbadb272121ea7b2da94007b
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 11:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let text = document.getElementById("evaluatedText");
+let counter = document.getElementById("letterCount");
+
+text.addEventListener('input',function(){
+	// document.write("hello")
+	let letters = this.value;
+	counter.textContent = letters.length;
+});
